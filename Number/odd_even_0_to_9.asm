014c1cdff634d908dc0b2396f1ecc3967216e2b1
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-02-27 07:30:38 +0600

    odd even 0 to 9

---
+++ Number/odd_even_0_to_9.asm
@@ -0,0 +1,50 @@
+org 100h
+.model small
+.stack 100h
+.data 
+MSG DB "Please input a digit from 0 to 9: $"
+MSG2 DB 10,13,"The DIGIT IS ODD $"
+MSG3 DB 10,13,"The DIGIT IS EVEN $"
+
+.code 
+MAIN PROC
+	MOV AX,@DATA
+	MOV DS,AX
+
+	MOV AH,9
+	LEA DX,MSG
+	INT 21H
+
+	MOV AH,1
+	INT 21H
+
+	SUB AL,'0'
+
+	MOV BL,AL
+	
+	AND BL,1
+	JZ EVEN
+	JMP ODD
+
+
+	
+
+	EVEN:
+		MOV AH,9
+		LEA DX,MSG3
+		
+		JMP STOP
+
+	ODD:
+		MOV AH,9
+		LEA DX,MSG2
+		
+		JMP STOP
+
+	STOP:
+		
+		INT 21H
+
+
+	MAIN ENDP
+END MAIN
